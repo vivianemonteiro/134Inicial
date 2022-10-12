571f7a67898913a2b669e2a612f4e46eed387e91
Viviane Monteiro <vivianemonteiro@yahoo.com.br>
2022-10-12 17:49:42 -0300

    Inclusão das operações aritméticas e criação de lista de valores para execução dos testes

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pytest
 
 
-def print_hi(name):
-    print(f'Hi, {name}')
+def imprimir_oi(nome):
+    print(f'Oi, {nome}')
 
 
 def somar(numero_a, numero_b):
     return numero_a + numero_b
-
 
 def dividir(numero_a, numero_b):
     try:
@@ -15,11 +14,20 @@ def dividir(numero_a, numero_b):
     except ZeroDivisionError:
         return 'Não dividiras por zero'
 
+def subtrair(numero_a, numero_b):
+    return numero_a - numero_b
+
+def multiplicar(numero_a, numero_b):
+    return numero_a * numero_b
+
 
 if __name__ == '__main__':
-    print_hi('Vivis')
+    imprimir_oi('Luciana')
 
-    resultado = somar(8, 7)
-    print(f'A soma é: {resultado}')
+    # Chamar a definição somar e mostrar o resultado
+    resultado = somar(7, -6)
+    print(f'A soma: {resultado}')
 
-
+    # Chamar a definição subtrair e mostrar o resultado
+    resultado = subtrair(25, 8)
+    print(f'A subtração: {resultado}')
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,45 +1,68 @@
-from main import somar, dividir
+import pytest
+
+from main import somar, subtrair, multiplicar, dividir
 
 
-class TesteContas:
-    def teste_somar(self):
-        # 1- Configurar
-        numero_a = 8
-        numero_b = 7
-        resultado_esperado = 15
+def teste_somar():
+    # 1- Configurar
+    numero_a = 8
+    numero_b = 7
+    resultado_esperado = 15
 
-        # 2- Executar
-        resultado_obtido = somar(numero_a, numero_b)
+    # 2- Executar
+    resultado_obtido = somar(numero_a, numero_b)
 
-        # 3- Validar
-        assert resultado_obtido == resultado_esperado
+    # 3- Validar
+    assert resultado_obtido == resultado_esperado
 
-    def teste_dividir_positivo(self):
-        # 1- Configurar
-        # 1.1 Dados de Entrada
-        numero_a = 27
-        numero_b = 3
 
-        # 1.2 Resultados Esperados
-        resultado_esperado = 9
+lista_de_valores = [
+    (8, 7, 15),
+    (20, 30, 50),
+    (25, 0, 25),
+    (-5, 12, 7),
+    (6, -3, 3)
+]
 
-        # 2- Executar
-        resultado_obtido = dividir(numero_a, numero_b)
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
+    # 1- Configurar
+    # utilizada a lista como massa de teste
 
-        # 3- Validar
-        assert resultado_obtido == resultado_esperado
+    # 2- Executar
+    resultado_obtido = somar(numero_a, numero_b)
 
-    def teste_dividir_negativo(self):
-        # 1- Configurar
-        # 1.1 Dados de Entrada
-        numero_a = 27
-        numero_b = 0
+    # 3- Validar
+    assert resultado_obtido == resultado_esperado
 
-        # 1.2 Resultados Esperados
-        resultado_esperado = 'Não dividiras por zero'
 
-        # 2- Executar
-        resultado_obtido = dividir(numero_a, numero_b)
+def teste_dividir_positivo():
+    # 1- Configurar
+    # 1.1 Dados de Entrada
+    numero_a = 27
+    numero_b = 3
 
-        # 3- Validar
-        assert resultado_obtido == resultado_esperado
+    # 1.2 Resultados Esperados
+    resultado_esperado = 9
+
+    # 2- Executar
+    resultado_obtido = dividir(numero_a, numero_b)
+
+    # 3- Validar
+    assert resultado_obtido == resultado_esperado
+
+
+def teste_dividir_negativo():
+    # 1- Configurar
+    # 1.1 Dados de Entrada
+    numero_a = 27
+    numero_b = 0
+
+    # 1.2 Resultados Esperados
+    resultado_esperado = 'Não dividiras por zero'
+
+    # 2- Executar
+    resultado_obtido = dividir(numero_a, numero_b)
+
+    # 3- Validar
+    assert resultado_obtido == resultado_esperado
