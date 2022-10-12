8bfe3f76959bb7aac640b42579745b344c8b4eb4
Viviane Monteiro <vivianemonteiro@yahoo.com.br>
2022-10-12 19:53:00 -0300

    Teste de unidade usando um arquivo CSV

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,6 +1,23 @@
+import csv
+
 import pytest
 
 from main import somar, subtrair, multiplicar, dividir
+
+
+def leitura_csv(arquivo_csv):
+    dados_csv = []
+    try:
+        with open(arquivo_csv, newline='') as massa:
+            campos = csv.reader(massa, delimiter=',')
+            next(campos)
+            for linha in campos:
+                dados_csv.append(linha)
+        return dados_csv
+    except FileNotFoundError:
+        print(f'Arquivo não encontrado: {arquivo_csv}')
+    except Exception as fail:
+        print(f'Falha imprevista: {fail}')
 
 
 def teste_somar():
@@ -24,6 +41,7 @@ lista_de_valores = [
     (6, -3, 3)
 ]
 
+
 @pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
 def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
     # 1- Configurar
@@ -34,6 +52,17 @@ def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
 
     # 3- Validar
     assert resultado_obtido == resultado_esperado
+
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', leitura_csv('C:\\Users\\A426692\\PycharmProjects\\134Inicial\\vendors\\csv\\massa_teste_somar_positivo.csv'))
+def teste_somar_leitura_de_csv(numero_a, numero_b, resultado_esperado):
+    # 1- Configurar
+    # utilizada a lista como massa de teste
+
+    # 2- Executar
+    resultado_obtido = somar(int(numero_a), int(numero_b))
+
+    # 3- Validar
+    assert resultado_obtido == int(resultado_esperado)
 
 
 def teste_dividir_positivo():
