636d44d84e738cc25621bae5da9e7d57a6a04b54
Viviane Monteiro <vivianemonteiro@yahoo.com.br>
2022-10-18 09:10:08 -0300

    Inclusão dos testes de venda e encapsulamento do caminho para abertura de arquivos

--- tests/api/test_store.py
+++ tests/api/test_store.py
@@ -1,7 +1,9 @@
 # Done: 3. Criar a venda de um pet para um usuário
 # ToDo: 4. Consultar os dados do pet que foi vendido
+import os.path
+
 import requests
-url = 'https://petstore.swagger.io/v2/store/order'
+url = 'https://petstore.swagger.io/v2/'
 headers = {'Content-Type': 'application/json'}
 
 def teste_vender_pet():
@@ -16,10 +18,11 @@ def teste_vender_pet():
     status_pedido_esperado = 'placed'
 
     # Executa
+    caminho = os.path.abspath(__file__ + "/../../../") + os.sep + 'vendors' + os.sep + 'json' + os.sep
     resultado_obtido = requests.post(
-        url=url,
+        url=url + 'store/order',
         headers=headers,
-        data=open('C:\\Users\\A426692\\PycharmProjects\\134Inicial\\vendors\\json\\pedido1.json')
+        data=open(caminho + 'pedido1.json')
     )
 
     # Valida
@@ -32,3 +35,27 @@ def teste_vender_pet():
     assert resultado_obtido['petId'] == pet_id_esperado
     assert corpo_do_resultado_obtido['status'] == status_pedido_esperado
 
+    # Extrair
+    pet_id_extraido = corpo_do_resultado_obtido.get('petId')
+
+    # Realizar a 2ª Transação
+
+    # Configura
+    # Dados de Entrada
+    # Extraídos da 1ª transação acima
+
+    # Reaultado Esperado
+    pet_name_esperado = 'Luke'
+    status_code_esperado = 200
+
+    # Executa
+    resultado_obtido = requests.get(
+        url=url + 'pet/' + str(pet_id_extraido),
+        headers=headers
+    )
+
+    # Validação
+    assert resultado_obtido.status_code == status_code_esperado
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    print(json.dumps(corpo_do_resultado_obtido, indent=4))
+    assert corpo_do_resultado_obtido['name'] == pet_name_esperado
