2feec9863ed1d3d4eaedda14c9dc03f9657c9786
Viviane Monteiro <vivianemonteiro@yahoo.com.br>
2022-10-24 17:11:36 -0300

    Inclusão de lista de massa de teste e de testes utilizando os navegadores firefox e edge

--- tests/web/selemium_webdriver.py
+++ tests/web/selemium_webdriver.py
@@ -1,30 +1,31 @@
 # Configura
 # Bibliotecas/Imports
+import pytest
 from selenium import webdriver
-
 
 # Dados de Entrada
 from selenium.webdriver.common.by import By
 
-origem = 'São Paolo'
-destino = 'Dublin'
+#origem = 'São Paolo'
+#destino = 'Dublin'
 primeiro_nome = 'Viviane'
 bandeira = 'American Express'
 lembrar_de_mim = True
 
 # Resultados Esperados
-titulo_passagens_esperado = 'Flights from São Paolo to Dublin:'
+#titulo_passagens_esperado = 'Flights from São Paolo to Dublin:'
 mensagem_agradecimento_esperada = 'Thank you for your purchase today!'
 preco_passagem_esperado = '555 USD'
 
 # Executa
 url = 'https://blazedemo.com/'
+
+
 class Testes:
     # Início
-    def setup_method(self):
+   # def setup_method(self):
         # instanciar a biblioteca/motor/engine
         # informar onde está o WebDriver
-        self.driver = webdriver.Edge()
 
 
     # Fim
@@ -32,28 +33,59 @@ class Testes:
         # destruir o objeto da biblioteca utilizado
         self.driver.quit()
 
+# Lista para uso como massa de teste
+    lista_de_valores = [
+        ('São Paolo', 'New York', 'firefox'),
+        ('Boston', 'Berlin', 'edge'),
+        ('San Diego', 'London', 'firefox'),
+        ('Philadelphia', 'Cairo', 'edge')
+    ]
+
+    @pytest.mark.parametrize('origem,destino, browser', lista_de_valores)
+
     # Meio
-    def testar_comprar_passagem(self):
+    def testar_comprar_passagem(self, origem, destino, browser):
         # e2e/end to end/ ponta a ponta
+        # Trouxe o setu_method / Iniciação para cá
+        match browser:
+            case 'edge':
+                self.driver = webdriver.Edge()
+            case 'firefox':
+                self.driver = webdriver.Firefox()
+
         # Página Inicial (Home)
         # Executa/Valida
         self.driver.get(url)
-        lista = self.driver.find_element(By.NAME('fromPort'))
+        lista = self.driver.find_element(By.NAME, 'fromPort')
         lista.click()
-        lista.find_element(By.XPATH('//option[ .="São Paolo"]')).click()
-        lista = self.driver.find_element(By.NAME('toPort'))
+        lista.find_element(By.XPATH, f'//option[ .= "{origem}"]').click()
+        lista = self.driver.find_element(By.NAME, 'toPort')
         lista.click()
-        lista.find_element(By.XPATH('//option[ .="Dublin"]')).click()
-        self.driver.find_element(By.CSS_SELECTOR('input.btn.btn-primary'))
-
-
+        lista.find_element(By.XPATH, f'//option[ .= "{destino}"]').click()
+        lista.click()
+        self.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
 
         # Página Lista de Passagens
-        # Executa/Valida
+        # Executa / Valida
+        assert self.driver.find_element(By.TAG_NAME, 'h3').text == f'Flights from {origem} to {destino}:'
+
+        self.driver.find_element(By.CSS_SELECTOR, 'tr:nth-child(3) .btn').click()
 
         # Página de Compra
         # Executa/Valida
+        # Preenche nome do comprador
+        self.driver.find_element(By.ID, 'inputName').send_keys(primeiro_nome)
+        # Seleciona a bandeira do cartão
+        lista = self.driver.find_element(By.ID, 'cardType')
+        lista.click()
+        lista.find_element(By.XPATH, f'//option[ .= "{bandeira}"]').click()
+        # Marca o checkbox para ser lembrado
+        self.driver.find_element(By.ID, 'rememberMe').click()
+        # Aperta o botão de Purchase Flight
+        self.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
 
         # Página de Obrigado
-        # Executa
         # Valida
+        assert self.driver.find_element(By.TAG_NAME, 'h1').text == mensagem_agradecimento_esperada
+        assert self.driver.find_element(By.CSS_SELECTOR, 'tr:nth-child(3) > td:nth-child(2)').text == \
+               preco_passagem_esperado
